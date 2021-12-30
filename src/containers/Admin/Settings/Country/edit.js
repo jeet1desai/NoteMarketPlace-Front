25610b37f989b8516a3d8752a71e850f46ea1ea0
jeet1desai <jeetdesai995@gmail.com>
2021-12-30 11:09:29 +0530

    update edit country page

--- src/containers/Admin/Settings/Country/edit.js
+++ src/containers/Admin/Settings/Country/edit.js
@@ -1,13 +1,49 @@
 import React from "react";
+import { Formik, Form } from "formik";
 
-import AdminHeader from "../../../../hoc/admin/header";
-import AdminFooter from "../../../../hoc/admin/footer";
+import "../../../../assets/css/add-country.css";
 
 export default function EditCountry() {
   return (
-    <>
-      <AdminHeader />
-      <AdminFooter />
-    </>
+    <div className="add-country">
+      <div className="container">
+        <div className="add-form">
+          <div className="page-title">
+            <p>Edit Country</p>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <Formik initialValues={{}} onSubmit={(values) => {}}>
+                <Form>
+                  <div className="form-group">
+                    <label for="title">Country Name *</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="title"
+                      placeholder="Enter Your Country Name "
+                      required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="title">Country Code *</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="title"
+                      placeholder="Enter Your Country Code"
+                      required
+                    />
+                  </div>
+                  <button type="button" className="btn submit-btn btn-purple">
+                    Submit
+                  </button>
+                </Form>
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
