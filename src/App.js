9f4f5ad51255b57a85377c147cd4cf547b4621fb
jeet1desai <jeetdesai995@gmail.com>
2024-01-01 19:52:58 +0530

    implement lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
@@ -6,37 +6,38 @@ import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "./assets/font-awesome/css/font-awesome.css";
-import "./assets/css";
+import "./css";
 
-import Login from "./containers/Auth/login";
-import Signup from "./containers/Auth/signup";
-import ForgetPassword from "./containers/Auth/forget-password";
-import EmailVerification from "./containers/Auth/email-verification";
-import ChangePassword from "./containers/Auth/change-password";
-import SuccessEmailVerification from "./containers/Auth/success-email-verify";
-
-import UserRoute from "./containers/User";
-import AdminRoute from "./containers/Admin";
-import PrivateRoute from "./components/Routes/Private";
+const PrivateRoute = lazy(() => import("./components/Routes/Private"));
+const Login = lazy(() => import("./containers/Auth/login"));
+const Signup = lazy(() => import("./containers/Auth/signup"));
+const ForgetPassword = lazy(() => import("./containers/Auth/forget-password"));
+const EmailVerification = lazy(() => import("./containers/Auth/email-verification"));
+const ChangePassword = lazy(() => import("./containers/Auth/change-password"));
+const SuccessEmailVerification = lazy(() => import("./containers/Auth/success-email-verify"));
+const UserRoute = lazy(() => import("./containers/User"));
+const AdminRoute = lazy(() => import("./containers/Admin"));
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/forget-password" component={ForgetPassword} />
-        <Route exact path="/email-verification" component={EmailVerification} />
-        <Route exact path="/change-password" component={ChangePassword} />
-        <Route exact path="/email/confirm/:id" component={SuccessEmailVerification} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/forget-password" component={ForgetPassword} />
+          <Route exact path="/email-verification" component={EmailVerification} />
+          <Route exact path="/change-password" component={ChangePassword} />
+          <Route exact path="/email/confirm/:id" component={SuccessEmailVerification} />
 
-        <PrivateRoute roles={[1, 2]} path="/admin" component={AdminRoute} />
+          <PrivateRoute roles={[1, 2]} path="/admin" component={AdminRoute} />
 
-        <Route path="/" component={UserRoute} />
+          <Route path="/" component={UserRoute} />
 
-        <Redirect to="/" />
-      </Switch>
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,60 +1,61 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Redirect } from "react-router-dom";
 
-import Dashboard from "./dashboard";
-import SpamReports from "./Reports/spam";
-import Members from "./Members/members";
-import MemberDetail from "./Members/member-detail";
-import Downloaded from "./Notes/downloaded";
-import Rejected from "./Notes/rejected";
-import Published from "./Notes/published";
-import UnderReview from "./Notes/under-review";
-import SystemConfig from "./Settings/system-config";
-import AdminProfile from "./Profile";
-import Admin from "./Settings/Admin";
-import Category from "./Settings/Category";
-import Country from "./Settings/Country";
-import Type from "./Settings/Type";
-import AdminFrom from "./Settings/Admin/admin-form";
-import CategoryForm from "./Settings/Category/category-form";
-import CountryForm from "./Settings/Country/country-form";
-import TypeForm from "./Settings/Type/type-form";
-import AdminNoteDetails from "./Notes/note-details";
-
-import AdminHeader from "../../hoc/admin/header";
-import AdminFooter from "../../hoc/admin/footer";
-import PrivateRoute from "../../components/Routes/Private";
+const AdminHeader = lazy(() => import("../../hoc/admin/header"));
+const AdminFooter = lazy(() => import("../../hoc/admin/footer"));
+const PrivateRoute = lazy(() => import("../../components/Routes/Private"));
+const Dashboard = lazy(() => import("./dashboard"));
+const SpamReports = lazy(() => import("./Reports/spam"));
+const Members = lazy(() => import("./Members/members"));
+const MemberDetail = lazy(() => import("./Members/member-detail"));
+const Downloaded = lazy(() => import("./Notes/downloaded"));
+const Rejected = lazy(() => import("./Notes/rejected"));
+const Published = lazy(() => import("./Notes/published"));
+const UnderReview = lazy(() => import("./Notes/under-review"));
+const SystemConfig = lazy(() => import("./Settings/system-config"));
+const AdminProfile = lazy(() => import("./Profile"));
+const Admin = lazy(() => import("./Settings/Admin"));
+const Category = lazy(() => import("./Settings/Category"));
+const Country = lazy(() => import("./Settings/Country"));
+const Type = lazy(() => import("./Settings/Type"));
+const AdminForm = lazy(() => import("./Settings/Admin/admin-form"));
+const CategoryForm = lazy(() => import("./Settings/Category/category-form"));
+const CountryForm = lazy(() => import("./Settings/Country/country-form"));
+const TypeForm = lazy(() => import("./Settings/Type/type-form"));
+const AdminNoteDetails = lazy(() => import("./Notes/note-details"));
 
 export default function AdminRoute() {
   return (
     <>
       <AdminHeader />
-      <Switch>
-        <PrivateRoute roles={[1, 2]} exact path="/admin/dashboard" component={Dashboard} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/note/:id" component={AdminNoteDetails} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/note-under-review" component={UnderReview} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/published-notes" component={Published} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/downloaded-notes" component={Downloaded} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/rejected-notes" component={Rejected} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/members" component={Members} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/members/:id" component={MemberDetail} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/spam-report" component={SpamReports} />
-        <PrivateRoute roles={[1]} exact path="/admin/manage-system-config" component={SystemConfig} />
-        <PrivateRoute roles={[1]} exact path="/admin/manage-admin" component={Admin} />
-        <PrivateRoute roles={[1]} exact path="/admin/manage-admin/add-admin" component={AdminFrom} />
-        <PrivateRoute roles={[1]} exact path="/admin/manage-admin/edit-admin/:id" component={AdminFrom} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category" component={Category} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category/add-category" component={CategoryForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category/edit-category/:id" component={CategoryForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type" component={Type} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type/add-type" component={TypeForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type/edit-type/:id" component={TypeForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country" component={Country} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country/add-country" component={CountryForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country/edit-country/:id" component={CountryForm} />
-        <PrivateRoute roles={[1, 2]} exact path="/admin/my-profile" component={AdminProfile} />
-        <Redirect to="/admin/dashboard" />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <PrivateRoute roles={[1, 2]} exact path="/admin/dashboard" component={Dashboard} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/note/:id" component={AdminNoteDetails} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/note-under-review" component={UnderReview} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/published-notes" component={Published} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/downloaded-notes" component={Downloaded} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/rejected-notes" component={Rejected} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/members" component={Members} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/members/:id" component={MemberDetail} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/spam-report" component={SpamReports} />
+          <PrivateRoute roles={[1]} exact path="/admin/manage-system-config" component={SystemConfig} />
+          <PrivateRoute roles={[1]} exact path="/admin/manage-admin" component={Admin} />
+          <PrivateRoute roles={[1]} exact path="/admin/manage-admin/add-admin" component={AdminForm} />
+          <PrivateRoute roles={[1]} exact path="/admin/manage-admin/edit-admin/:id" component={AdminForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category" component={Category} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category/add-category" component={CategoryForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-category/edit-category/:id" component={CategoryForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type" component={Type} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type/add-type" component={TypeForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-type/edit-type/:id" component={TypeForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country" component={Country} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country/add-country" component={CountryForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/manage-country/edit-country/:id" component={CountryForm} />
+          <PrivateRoute roles={[1, 2]} exact path="/admin/my-profile" component={AdminProfile} />
+          <Redirect to="/admin/dashboard" />
+        </Switch>
+      </Suspense>
       <AdminFooter />
     </>
   );
